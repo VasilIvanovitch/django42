37c7697d3e101eb5384a4b26ce084a88c5925d5f
VasilIvanovitch <vnechviadovich@gmail.com>
2023-09-04 21:12:11 +0300

    примеры динамических URL и пользовательских конвертеров. урок 6

--- django/sitewomen/women/urls.py
+++ django/sitewomen/women/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
-from . import views
+from . import views, converters
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', views.index),
     path('cats/', views.categoris),
+    path('cats/<int:cat_id>/', views.categoris_id),
+    path('cats/<slug:cat_slug>/', views.categoris_by_slug),
+    path("archive/<year4:year>/", views.archive),
+    # re_path(r"^archive/(?P<year>[0-9]{4})/", views.archive),
+
 ]
--- django/sitewomen/women/views.py
+++ django/sitewomen/women/views.py
@@ -6,4 +6,15 @@ def index(request):
     return HttpResponse("Страница приложения Women")
 
 def categoris(request):
-    return HttpResponse("<h1>Статьи по категориям   </h1>")
+    return HttpResponse("<h1>Статьи по категориям</h1>")
+
+def categoris_id(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>Категория id: {cat_id}</p>")
+
+
+def categoris_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>Категория slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>год: {year}</p>")
