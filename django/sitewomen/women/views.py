e91780377d842b82705c24805514d40a5fd6ab2f
VasilIvanovitch <vnechviadovich@gmail.com>
2023-09-09 15:18:44 +0300

    примеры фильтров и тегов, урок 12-12

--- django/sitewomen/women/views.py
+++ django/sitewomen/women/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
 
 
 menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Выйти']
 
+
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
+
+
 def index(request):
     data = {'title': 'Главная страница',
-            'menu': menu}
+            'menu': menu,
+            'posts': data_db,
+            'url': slugify('Margot Robbie')}
     return render(request, 'women/index.html', context=data)
     # st = render_to_string('women/index.html')
     # return HttpResponse(st)
