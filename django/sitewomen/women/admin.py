2cd1473f6faab48806088f7a8388ae5128147fc6
VasilIvanovitch <vnechviadovich@gmail.com>
2023-10-11 19:27:48 +0300

    Пользовательские поля и действия в админ-панели, урок 39

--- django/sitewomen/women/admin.py
+++ django/sitewomen/women/admin.py
@@ -1,17 +1,34 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
 from women.models import Women, Category, TagPosts, Husband
 
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'cat', 'is_published')
-    list_display_links = ('id', 'title')
+    list_display = ('title', 'time_create', 'cat', 'is_published', 'brief_info')
+    list_display_links = ('title',)
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {"slug": ("title",)}
     list_per_page = 7
+    actions = ['set_published', 'set_draft']
+
+
+    @admin.display(description='Краткое описание', ordering='content')
+    def brief_info(self, women: Women):
+        return f'Описание {len(women.content)} символов'
+
+
+    @admin.action(description='Опубликовать выбранные записи')
+    def set_published(self, request, queryset):
+            count = queryset.update(is_published=Women.Status.PUBLISHED)
+            self.message_user(request, f'Изменено {count} записей.')
+
+    @admin.action(description='Снять с публикации выбранные записи')
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f'{count} записей снято с публикации.', messages.WARNING)
 
 
 @admin.register(Category)
