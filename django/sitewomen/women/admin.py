890caee9c83746c39fae451cc9f96cbab48258da
VasilIvanovitch <vnechviadovich@gmail.com>
2023-10-13 21:57:26 +0300

    Пример панели поиска и панели фильтрации в админ-панели, урок 40

--- django/sitewomen/women/admin.py
+++ django/sitewomen/women/admin.py
@@ -3,13 +3,30 @@ from django.contrib import admin, messages
 from women.models import Women, Category, TagPosts, Husband
 
 
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Статус женщин'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужем'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
+
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     list_display = ('title', 'time_create', 'cat', 'is_published', 'brief_info')
     list_display_links = ('title',)
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
-    list_filter = ('is_published', 'time_create')
+    list_filter = (MarriedFilter, 'is_published', 'cat__name')
     prepopulated_fields = {"slug": ("title",)}
     list_per_page = 7
     actions = ['set_published', 'set_draft']
@@ -18,7 +35,6 @@ class WomenAdmin(admin.ModelAdmin):
     @admin.display(description='Краткое описание', ordering='content')
     def brief_info(self, women: Women):
         return f'Описание {len(women.content)} символов'
-
 
     @admin.action(description='Опубликовать выбранные записи')
     def set_published(self, request, queryset):
