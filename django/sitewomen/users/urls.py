fdd4d27703577f15df1b3c8f268336be56267525
VasilIvanovitch <vnechviadovich@gmail.com>
2023-11-11 15:15:17 +0300

    пример class PasswordChangeView и PasswordChangeDoneView, урок 67

--- django/sitewomen/users/urls.py
+++ django/sitewomen/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path, register_converter
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeDoneView
 
 from . import views
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUser.as_view(), name='profile'),
+    path('password-change/', views.UserPasswordChange.as_view(), name='password_change'),
+    path('password-change/done/', views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
 ]
--- django/sitewomen/users/views.py
+++ django/sitewomen/users/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordChangeDoneView
 from django.views.generic import CreateView, UpdateView
-from .forms import LoginUserForm, RegisterUserForm, ProfileUserForm
+from .forms import LoginUserForm, RegisterUserForm, ProfileUserForm, UserPasswordChangeForm
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -37,6 +37,15 @@ class ProfileUser(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+class UserPasswordChange(PasswordChangeView):
+    form_class = UserPasswordChangeForm
+    success_url = reverse_lazy("users:password_change_done")
+    template_name = "users/password_change_form.html"
+    extra_context = {'title': "Изменение пароля"}
+
+
 
 # def register(request):
 #     if request.method == 'POST':
