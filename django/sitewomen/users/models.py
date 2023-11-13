ddce17a1aa5ca978a1344e1ab3370533373dc715
VasilIvanovitch <vnechviadovich@gmail.com>
2023-11-13 19:42:41 +0300

    создание модели Profile, урок 71

--- django/sitewomen/users/models.py
+++ django/sitewomen/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE,
+                                related_name='user', verbose_name='User')
+    photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True, null=True, verbose_name="Фотография")
+    date_birth = models.DateTimeField(blank=True, null=True, verbose_name="Дата рождения")
