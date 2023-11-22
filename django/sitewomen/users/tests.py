8b66343716f403dbaa09317e9147c918b43fd789
VasilIvanovitch <vnechviadovich@gmail.com>
2023-11-22 18:30:45 +0300

    примеры unittest тестов в приложении users, урок 85

--- django/sitewomen/users/tests.py
+++ django/sitewomen/users/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
+
 class RegisterUserTestCase(TestCase):
     def setUp(self):
         self.data = {
